f17143dbb1eadb09930dde71817490d0963f7805
Louis-Saglio <louis.saglio@sfr.fr>
2017-12-29 00:30:27 +0100

    Script to display data in a graph using matplotlib.

---
+++ graphic.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+
+with open('data', 'r') as f:
+    lines = f.readlines()
+
+teutons = []
+britons = []
+for line in lines:
+    ti, bi = line[:-1].split(',')
+    teutons.append(int(ti))
+    britons.append(int(bi))
+
+y = list(range(len(teutons)))
+plt.plot(y, teutons, label='Teutons')
+plt.plot(y, britons, label='Britons')
+plt.xlabel('Time')
+plt.ylabel('Number of vote')
+plt.legend()
+plt.savefig("images.png")
+plt.show()
