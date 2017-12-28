64eee93af830365bef928bee978463df4b477d2d
Louis-Saglio <louis.saglio@sfr.fr>
2017-12-28 22:42:44 +0100

    Add main loop Fix bug : static instead of dynamic

--- data_getter.py
+++ data_getter.py
@@ -1,19 +1,26 @@
-import requests
-import secret
-import settings
 import json
 import pprint
 
+import requests
+import time
+
+import secret
+import settings
+
 
 class Parser:
-    url = f"https://graph.facebook.com/v2.11/{settings.POST_ID}/{settings.DATA_TYPE}?access_token={secret.TOKEN}"
-    data = dict()
 
-    def main(self, log=True):
+    def __init__(self):
+        self.url = f"https://graph.facebook.com/v2.11/{settings.POST_ID}/{settings.DATA_TYPE}?access_token={secret.TOKEN}"
+        self.data = dict()
+        self.log = {"url": False, "end": True}
+
+    def main(self):
         while self.url is not None:
-            if log:
+            if self.log["url"]:
                 print(self.url)
             self.parse(json.loads(requests.get(self.url).content))
+        return self.data
 
     def parse(self, response: dict):
         for datum in response["data"]:
@@ -25,7 +32,14 @@ class Parser:
         self.url = response["paging"].get("next")
 
     def __del__(self):
-        pprint.pprint(self.data)
+        if self.log["end"]:
+            pprint.pprint(self.data)
 
 
-Parser().main()
+while True:
+    time.sleep(5)
+    data = Parser().main()
+    teutons = data["HAHA"]
+    britons = data["LOVE"]
+    with open("data", "a") as f:
+        f.write(f"{teutons},{britons}\n")
