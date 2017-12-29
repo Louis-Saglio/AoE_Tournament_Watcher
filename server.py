ccb7b4a4b6ec4b3c6d27b8ac2738f012730eba97
Louis-Saglio <louis.saglio@sfr.fr>
2017-12-29 01:03:58 +0100

    Refresh graphic for each request Refactor graphic to be used in other script

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 import flask
+import graphic
 
 app = flask.Flask("AoE_Meme_Tournament")
 
 
 @app.route("/")
 def home():
+    graphic.main(show=False)
     return flask.render_template("image.html")
